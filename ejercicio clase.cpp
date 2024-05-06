f8309f7426f5b34b03f66d67d36bd8ea310882c7
hugxmaldonado <164410198+hugxmaldonado@users.noreply.github.com>
2024-05-06 12:45:16 -0600

    Add files via upload

---
+++ ejercicio clase.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 1, resultado=0;
+    while (i<13)
+{
+    resultado=i*12;
+
+    cout << "12 *" <<i<< " = "<<resultado <<endl;
+    i++;
+}
+return 0;
+}
